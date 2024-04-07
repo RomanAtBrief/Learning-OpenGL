8fee99e4b03745802f7f4623d3cfe6a09fc2e64d
Roman Gorshkov <roman@thebrief.space>
2024-04-07 10:26:53 +0100

    Initial Commit

---
+++ 01_OpenGL_Window/src/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * File: main.cpp
+ * ---------------------
+ * Description
+ */
+
+/* Preprocessor statement always satrts with # */
+#include <iostream>
+
+/* Main program */
+int main()
+{
+    std::cout << "Hello, world" << std::endl;
+    return 0;
+}
