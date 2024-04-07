aac3be085cd7983eab79b77d8e4be22088d67bba
Roman Gorshkov <roman@thebrief.space>
2024-04-07 12:12:41 +0100

    GLFW Setup and Running

--- 01_OpenGL_Window/src/main.cpp
+++ 01_OpenGL_Window/src/main.cpp
@@ -24,6 +24,54 @@
 /* Main program */
 int main()
 {
-    std::cout << "Hello, world" << std::endl;
+    /* Initialize the library */
+    if (!glfwInit())
+    {
+        /* Library initialization failed */
+        std::cout << "Error, Library initialization failed." << std::endl;
+    }
+    else
+    {
+        /* Library initialization succeeded */
+        std::cout << "Library initialization succeeded." << std::endl;
+    }
+
+    /* Creating window */
+    GLFWwindow *window = glfwCreateWindow(640, 480, "OpenGL Window", NULL, NULL);
+    if (!window)
+    {
+        /* Window or OpenGL context creation failed */
+        std::cout << "Error creating window!" << std::endl;
+        glfwTerminate();
+    }
+    else
+    {
+        /* Window and OpenGL context creation succeeded */
+        std::cout << "Window and OpenGL context creation succeeded." << std::endl;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Set background window colour */
+    glClearColor(0.85, 0.85, 0.85, 1);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    /* This destroys any remaining windows and releases any other resources allocated by GLFW */
+    glfwTerminate();
+    std::cout << "GLFW Window was closed and terminated" << std::endl;
+
     return 0;
 }
