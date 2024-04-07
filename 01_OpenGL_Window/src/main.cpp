c11b74f89cd30aeba9f7d00b3a506ea64fcf8ce4
Roman Gorshkov <roman@thebrief.space>
2024-04-07 11:21:03 +0100

    Added GLFW

--- 01_OpenGL_Window/src/main.cpp
+++ 01_OpenGL_Window/src/main.cpp
@@ -1,11 +1,25 @@
 /*
  * File: main.cpp
  * ---------------------
- * Description
+ * This is a main program that creates window using GLFW
+ * Graphics Library Framework
  */
 
 /* Preprocessor statement always satrts with # */
 #include <iostream>
+
+/*
+ * Include GLEW - OpenGL Extension Wranler.
+ * It responsible for loading OpenGL extensions at run-time
+ */
+// #include <GLEW/glew.h>
+
+/*
+ * Include GLFW - Graphics Library Framework.
+ * It provides a simple API for creating windows, contexts and surfaces,
+ * receiving input and events.
+ */
+// #include <GLFW/glfw3.h>
 
 /* Main program */
 int main()
